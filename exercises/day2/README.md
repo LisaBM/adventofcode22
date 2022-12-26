32720317eb39b8b6702b599ddc48dca56592b822
LisaBM <lmattrisch@msn.com>
2022-12-26 23:45:52 +0100

    Day 2: Rock Paper Scissors (#3)
    * Day 2: Rock Paper Scissors
    
    * day 2: improved README and docstrings

---
+++ exercises/day2/README.md
@@ -0,0 +1,15 @@
+# Day 2: Rock Paper Scissors
+
+https://adventofcode.com/2022/day/2
+
+## Usage
+
+Start a development container. 
+
+Inside the container, run 
+```bash
+cd exercises/day2
+python strategy_score.py -f <name of input file>
+```
+
+The solution to you input file will be logged in the terminal.
---
+++ exercises/day2/strategy_score.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+from typing import Iterator, List
+
+
+class RPCChoice:
+    """Class representing a choice for a round of Rock, Paper, Scissors."""
+
+    def __init__(self, value: str) -> None:
+        match value:
+            case "A" | "X" | "rock":
+                self.value = "rock"
+                self.score = 1
+            case "B" | "Y" | "paper":
+                self.value = "paper"
+                self.score = 2
+            case "C" | "Z" | "scissors":
+                self.value = "scissors"
+                self.score = 3
+            case _:
+                raise ValueError(f"Value {value} unknown.")
+        
+    def __gt__(self, __o) -> bool:
+        return (self.score - __o.score) % 3 == 1
+
+    def __eq__(self, __o) -> bool:
+        return self.score == __o.score
+
+    def __lt__(self, __o) -> bool:
+        return (__o.score - self.score) % 3 == 1
+
+
+def strategy_score(filename: str) -> int:
+    """Compute the score of a strategy provided in a text file."""
+    with open(filename, "r") as f:
+        file_content = f.read().splitlines()
+
+    scores = _score_per_round(file_content)
+
+    return sum(scores)
+
+
+def _score_per_round(file_content: List[str]) -> Iterator[int]:
+    """Compute the score of a single round of Rock, Paper, Scissors."""
+    for line in file_content:
+        (opponents_choice_str, own_choice_str) = line.split(" ")
+        opponents_choice = RPCChoice(opponents_choice_str)
+        own_choice = RPCChoice(own_choice_str)
+        choice_score = own_choice.score
+        
+        yield choice_score + _outcome_score(opponents_choice, own_choice)
+
+
+def _outcome_score(opponents_choice: RPCChoice, own_choice: RPCChoice) -> int:
+    """Compute the score related to the outcome of one round."""
+    if own_choice > opponents_choice:
+        return 6
+    elif own_choice < opponents_choice:
+        return 0
+    else:
+        return 3
+
+
+def run() -> None:
+    args = parse_args()
+    logging.info(strategy_score(args.filename))
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--filename", "-f", dest="filename", type=str, required=True)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="INFO")
+    run()
