08dc31c7fa9e2278ba4f7465d00d75fdb8fe2d19
LisaBM <lmattrisch@msn.com>
2022-12-28 23:21:08 +0100

    brush up day1: typechecks, main and better README (#4)
    * brush up day1: typechecks, main and better README
    
    * newline for gitignore

--- exercises/day1/README.md
+++ exercises/day1/README.md
@@ -1,3 +1,15 @@
 # Day 1: Calorie Counting
 
 https://adventofcode.com/2022/day/1
+
+## Usage
+
+Start a development container. 
+
+Inside the container, run 
+```bash
+cd exercises/day2
+python most_calories.py -f <name of input file>
+```
+
+The solution to you input file will be logged in the terminal.
--- exercises/day1/most_calories.py
+++ exercises/day1/most_calories.py
@@ -1,4 +1,10 @@
-def most_calories(filename):
+import argparse
+import logging
+from typing import Iterator, List
+
+
+def most_calories(filename: str) -> int:
+    """Return the sum of calories the elf with the most calories carries."""
 
     with open(filename, "r") as f:
         file_content = f.read().splitlines()
@@ -7,7 +13,7 @@ def most_calories(filename):
     return max(elf_calories)
 
 
-def _calories_per_elf(file_content):
+def _calories_per_elf(file_content: List[str]) -> Iterator[int]:
     elf_calories = 0
     for line in file_content:
         if line != "":
@@ -15,3 +21,23 @@ def _calories_per_elf(file_content):
         else:
             yield elf_calories
             elf_calories = 0
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename",
+                        "-f",
+                        dest="filename",
+                        type=str,
+                        required=True)
+    return parser.parse_args()
+
+
+def run() -> None:
+    args = parse_args()
+    logging.info(most_calories(args.filename))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="INFO")
+    run()
