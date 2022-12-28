bb7056ded1ee52e76a513207e24120afc2b8a1af
LisaBM <lmattrisch@msn.com>
2022-12-29 00:02:02 +0100

    day3: implementation

--- exercises/day3/item_prioritization.py
+++ exercises/day3/item_prioritization.py
@@ -1,13 +1,46 @@
 import argparse
 import logging
+import string
+from types import MappingProxyType
 from typing import Iterator, List
+
+ALPHABET = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+PRIORITIES = MappingProxyType(
+    {item: priority for (item, priority) in zip(ALPHABET, range(1, 52 + 1))})
 
 
 def summed_priorities(filename: str) -> int:
     """Compute the sum of priorities of miscategorized items 
     given the rucksack contents in a text file.
     """
-    pass
+    with open(filename, "r") as f:
+        file_content = f.read().splitlines()
+
+    miscategorized_items = get_mistakes(file_content)
+    priorities = map_items_to_priorities(miscategorized_items)
+
+    return sum(priorities)
+
+
+def get_mistakes(file_content: List[str]) -> Iterator[str]:
+    for rucksack_content in file_content:
+        rucksack_content = rucksack_content.strip(" ")
+        n_items = len(rucksack_content)
+        assert n_items % 2 == 0, "Uneven amount of items."
+
+        compartment0 = set(rucksack_content[:int(n_items / 2)])
+        compartment1 = set(rucksack_content[int(n_items / 2):])
+
+        miscategorized_items = compartment0 & compartment1
+        assert (len(miscategorized_items) < 2,
+                "More than one miscateorized item.")
+
+        yield list(miscategorized_items)[0]
+
+
+def map_items_to_priorities(items: Iterator[str]) -> Iterator[int]:
+    for item in items:
+        yield PRIORITIES[item] 
 
 
 def run() -> None:
@@ -17,8 +50,11 @@ def run() -> None:
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--filename", "-f", dest="filename", type=str, required=True)
+    parser.add_argument("--filename",
+                        "-f",
+                        dest="filename",
+                        type=str,
+                        required=True)
     return parser.parse_args()
 
 
