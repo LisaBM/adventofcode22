228e5eebc238fef3d1b9e6dc09cbf562170d8d6d
LisaBM <lmattrisch@msn.com>
2022-12-28 23:37:51 +0100

    day3: test case

--- exercises/day3/tests/test_item_prioritization.py
+++ exercises/day3/tests/test_item_prioritization.py
@@ -1,3 +1,16 @@
-import pytest
-
 import item_prioritization
+
+
+def test_summed_priorities(tmp_path):
+    d = tmp_path
+    filename = d / "temp.txt"
+    with open(filename, "w") as f:
+        f.write("vJrwpWtwJgWrhcsFMMfFFhFp\n")
+        f.write("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n")
+        f.write("PmmdzqPrVvPwwTWBwg\n")
+        f.write("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n")
+        f.write("ttgJtRGJQctTZtZT\n")
+        f.write("CrZsJsPPZsGzwwsLwLmpwMDw\n")
+
+    assert item_prioritization.summed_priorities(filename) == 157
+    
